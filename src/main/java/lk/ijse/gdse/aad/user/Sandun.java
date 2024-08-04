147fbb56ebd407cc72c9838166b5a57504491e52
ThanuraSilva <thanurasilva1989@gmail.com>
2024-08-04 15:51:20 +0530

    Implemented constructor injection

--- src/main/java/lk/ijse/gdse/aad/user/Sandun.java
+++ src/main/java/lk/ijse/gdse/aad/user/Sandun.java
@@ -3,20 +3,22 @@ package lk.ijse.gdse.aad.user;
 import jakarta.annotation.PostConstruct;
 import lk.ijse.gdse.aad.dep.GoodGirl;
 import lk.ijse.gdse.aad.dep.Wow;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Sandun {
-    @Autowired
     @Qualifier("Long hair")
-    @Wow
-   private GoodGirl goodGirl;
 
-//   public Sandun(){
-//       System.out.println(goodGirl);
-//   }
+   private final GoodGirl goodGirl;
+    @Autowired
+    @Wow
+   public Sandun(GoodGirl goodGirl){
+        this.goodGirl = goodGirl;
+   }
+
    @PostConstruct
    public void init(){
        //System.out.println(goodGirl);
