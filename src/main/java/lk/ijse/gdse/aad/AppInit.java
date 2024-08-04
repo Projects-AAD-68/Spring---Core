6531fd66df8ace1878e038cd2fc8a32b9ebe397a
ThanuraSilva <thanurasilva1989@gmail.com>
2024-08-04 10:08:46 +0530

    Added missed AppInit

---
+++ src/main/java/lk/ijse/gdse/aad/AppInit.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad;
+
+import lk.ijse.gdse.aad.config.Config;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInit {
+    public static void main(String[] args) {
+        var ctx = new AnnotationConfigApplicationContext();
+        ctx.register(Config.class);
+        ctx.refresh();
+        ctx.close();
+    }
+}
